fcb31f6282d403f5295f970b07243d0aa532dfa7
Job Cespedes Ortiz <jobcespedes@gmail.com>
2019-12-26 14:28:14 -0600

    ubi image for php and apache

---
+++ build/quay.io/krestomatio/moodle_apache_php/conf/php-fpm-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+# php timezone
+if ! [ -z ${TZ+x} ]; then
+    echo "date.timezone = ${TZ}" > /usr/local/etc/php/conf.d/php-timezone.ini
+fi
+
+# Moodle
+if ! [ -z ${POSTGRES_DB+x} ] && ! [ -z ${POSTGRES_USER+x} ] && ! [ -z ${POSTGRES_PASSWORD+x} ] && ! [ -z ${MOODLE_DATA+x} ]; then
+    if [[ $WWW_PORT == '80' ]]; then
+        host_port="${WWWROOT}"
+    else
+        host_port="${WWWROOT}:${WWW_PORT}"
+    fi
+    cp -p ${DOCUMENT_ROOT}/config-dist.php ${DOCUMENT_ROOT}/config.php
+    sed -i \
+        -e  "s@^\$CFG->dbhost.*@\$CFG->dbhost    = 'postgres';@" \
+        -e  "s@^\$CFG->dbname.*@\$CFG->dbname    = '${POSTGRES_DB}';@" \
+        -e  "s@^\$CFG->dbuser.*@\$CFG->dbuser    = '${POSTGRES_USER}';@" \
+        -e  "s@^\$CFG->dbpass.*@\$CFG->dbpass    = '${POSTGRES_PASSWORD}';@" \
+        -e  "s@^\$CFG->wwwroot.*@\$CFG->wwwroot    = 'http://${host_port}';@" \
+        -e  "s@^\$CFG->dataroot.*@\$CFG->dataroot    = '${MOODLE_DATA}';@" \
+        ${DOCUMENT_ROOT}/config.php
+fi
+
+# Custom CA
+if [ -f /usr/local/share/ca-certificates/custom-ca.crt ]; then
+    update-ca-certificates
+    if [ -f /usr/local/lib/python2.7/dist-packages/certifi/cacert.pem ]; then
+        rm /usr/local/lib/python2.7/dist-packages/certifi/cacert.pem
+        ln -s /usr/local/share/ca-certificates/custom-ca.crt /usr/local/lib/python2.7/dist-packages/certifi/cacert.pem
+    fi
+fi
+
+docker-php-entrypoint "$@"
--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
-# TODO: moodle source code 
+# TODO: moodle source code
 # TODO: image development and production version
 ## php-fpm development version needs xdebug and profiling
+# TODO: delete php pspell and aspell pckg: https://moodle.org/mod/forum/discuss.php?d=232297
+# TODO: defined SO binaries require by Moodle
